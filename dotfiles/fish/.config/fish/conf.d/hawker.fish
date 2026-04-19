1e0a2b142855074e96574aa1fdded3f596dc3731
hinriksnaer <hgudmund@redhat.com>
2026-04-19 03:17:28 -0400

    Remove hawker.fish: HAWKER_PATH set by Nix everywhere

--- dotfiles/fish/.config/fish/conf.d/hawker.fish
+++
@@ -1,7 +0,0 @@
-# Hawker environment setup
-# HAWKER_PATH is set by NixOS session variables (modules/core/hawker-scripts.nix)
-# This file is kept as a fallback for non-NixOS environments (containers, remote hosts)
-
-if test -z "$HAWKER_PATH"
-    set -gx HAWKER_PATH "$HOME/.local/share/hawker"
-end
