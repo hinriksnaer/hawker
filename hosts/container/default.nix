2f2bfe7df78be5709909693dbfa09b02661f6d94
hinriksnaer <hgudmund@redhat.com>
2026-04-19 22:48:02 -0400

    Import all project options.nix in container config
    Fixes 'option does not exist' error when a project is defined in
    settings.nix but not in container.projects list. Same pattern as
    hosts/desktop/default.nix.

--- hosts/container/default.nix
+++ hosts/container/default.nix
@@ -8,20 +8,27 @@ let
 
   # Auto-discover projects: any directory in projects/ with a default.nix
   projectsDir = ../../projects;
-  allProjects = builtins.attrNames (
-    lib.filterAttrs (name: type:
-      type == "directory" && builtins.pathExists (projectsDir + "/${name}/default.nix")
-    ) (builtins.readDir projectsDir)
-  );
+  allProjectDirs = lib.filterAttrs (name: type:
+    type == "directory" && builtins.pathExists (projectsDir + "/${name}/default.nix")
+  ) (builtins.readDir projectsDir);
+  allProjects = builtins.attrNames allProjectDirs;
 
   enabledProjects = builtins.filter (p: builtins.elem p allProjects) projectsList;
   enabledModules = map (p: projectsDir + "/${p}") enabledProjects;
+
+  # Import all project options.nix so settings.nix can define values for
+  # disabled projects without causing "option does not exist" errors.
+  allProjectOptions = lib.mapAttrsToList
+    (name: _: projectsDir + "/${name}/options.nix")
+    (lib.filterAttrs (name: _:
+      builtins.pathExists (projectsDir + "/${name}/options.nix")
+    ) allProjectDirs);
 in
 {
   imports = [
     ../../modules/core
     ../../modules/terminal
-  ] ++ enabledModules;
+  ] ++ enabledModules ++ allProjectOptions;
 
   # Container-specific: no bootloader, no hardware
   boot.loader.systemd-boot.enable = lib.mkForce false;
