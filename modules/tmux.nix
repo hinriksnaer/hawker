4581f3bd458a628eeebbded574a003b9318fe906
hinriksnaer <hgudmund@redhat.com>
2026-04-23 21:13:02 -0400

    Move tmux config to stow dotfiles, simplify NixOS module to packages only
    Tmux config lives in dotfiles/tmux/ (deployed by bootstrap.sh via stow).
    NixOS module just provides tmux + plugins as system packages.
    Plugins are Nix-managed (no TPM), config references them via stow.

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,57 +1,12 @@
+# Tmux package + plugins. Config is in dotfiles/tmux/ (stow-managed).
 { pkgs, ... }:
 
 {
-  programs.tmux = {
-    enable = true;
-    keyMode = "vi";
-    shortcut = "Space";
-    baseIndex = 1;
-    terminal = "xterm-256color";
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      vim-tmux-navigator
-      yank
-      dotbar
-    ];
-    extraConfigBeforePlugins = ''
-      # Dotbar theme
-      set -g @tmux-dotbar-position top
-      set -g @tmux-dotbar-fg "colour8"
-      set -g @tmux-dotbar-bg "default"
-      set -g @tmux-dotbar-fg-current "colour7"
-      set -g @tmux-dotbar-fg-session "colour8"
-      set -g @tmux-dotbar-fg-prefix "colour14"
-    '';
-    extraConfig = ''
-      # Mouse support
-      set -g mouse on
-
-      set-option -sa terminal-overrides ",xterm*:Tc"
-      set -g pane-base-index 1
-      set-option -g renumber-windows on
-
-      # Vim style pane selection
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # Shift arrow to switch windows
-      bind -n S-Left  previous-window
-      bind -n S-Right next-window
-
-      # Shift Alt vim keys to switch windows
-      bind -n M-H previous-window
-      bind -n M-L next-window
-
-      # Copy mode
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      # Splits in current path
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-    '';
-  };
+  environment.systemPackages = with pkgs; [
+    tmux
+    tmuxPlugins.sensible
+    tmuxPlugins.vim-tmux-navigator
+    tmuxPlugins.yank
+    tmuxPlugins.dotbar
+  ];
 }
