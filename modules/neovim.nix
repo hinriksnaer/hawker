55233cc33f40acdf7166deffe28c764426da0508
hinriksnaer <hgudmund@redhat.com>
2026-04-25 22:00:14 -0400

    Add debugpy to neovim module for Python DAP support

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -7,6 +7,7 @@
     nodejs      # required by Copilot
     clang-tools # clangd + clang-format
     pyright     # Python LSP (Nix-provided, not Mason)
+    python3Packages.debugpy # Python DAP adapter
   ];
 
   # Set neovim as default editor
