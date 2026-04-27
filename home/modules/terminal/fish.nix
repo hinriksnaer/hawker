d70d8ed983d47d26f82eed333ccb329f42fa1578
hinriksnaer <hgudmund@redhat.com>
2026-04-27 19:24:00 -0400

    Set fish as default shell via bash exec on non-NixOS hosts

--- home/modules/terminal/fish.nix
+++ home/modules/terminal/fish.nix
@@ -1,10 +1,17 @@
-# Fish shell -- minimal HM config to enable shell integrations.
-# This enables programs.starship, programs.fzf, programs.zoxide, etc.
-# to generate their fish init files (~/.config/fish/conf.d/).
-# The full fish config (vi mode, venv, aliases) remains in the NixOS
-# module (modules/core/fish.nix) until fully migrated.
-{ ... }:
+# Fish shell -- enables shell integrations and sets fish as default.
+{ pkgs, ... }:
 
 {
   programs.fish.enable = true;
+
+  # Set fish as the default shell for interactive sessions.
+  # On non-NixOS hosts (where chsh may not work with Nix paths),
+  # this sources fish from bash/zsh login.
+  programs.bash.enable = true;
+  programs.bash.initExtra = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]; then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
 }
