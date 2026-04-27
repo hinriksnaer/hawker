6a6f582afef06038f2a012b3e69b2e8bca6ea89f
hinriksnaer <hgudmund@redhat.com>
2026-04-27 11:50:54 -0400

    Fix /hawker permission denied: map host uid to container uid 1000
    Rootless podman remaps uids, causing host-owned files to appear as
    a different uid inside the container. --userns=keep-id maps the host
    user to uid 1000 (the container's dev user) so bind-mounted dirs
    are writable.

--- containers/hawker-container.sh
+++ containers/hawker-container.sh
@@ -67,6 +67,7 @@ start_container() {
     $runtime run -it \
         --name "$IMAGE_NAME" \
         --hostname "$IMAGE_NAME" \
+        --userns=keep-id:uid=1000,gid=1000 \
         -v "${FLAKE_REF}:/home/${user}/hawker" \
         -v "${IMAGE_NAME}-repos:/home/${user}/repos" \
         -v "${IMAGE_NAME}-ccache:/home/${user}/.cache/ccache" \
