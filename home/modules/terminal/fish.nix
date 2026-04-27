cbd9af1914c701c91bd90b6c97373a55ab399153
hinriksnaer <hgudmund@redhat.com>
2026-04-27 19:27:19 -0400

    Add zsh → fish exec for non-NixOS hosts with zsh as default shell

--- home/modules/terminal/fish.nix
+++ home/modules/terminal/fish.nix
@@ -6,12 +6,19 @@
 
   # Set fish as the default shell for interactive sessions.
   # On non-NixOS hosts (where chsh may not work with Nix paths),
-  # this sources fish from bash/zsh login.
+  # exec into fish from both bash and zsh login shells.
   programs.bash.enable = true;
   programs.bash.initExtra = ''
     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]; then
       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+
+  programs.zsh.enable = true;
+  programs.zsh.initExtra = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${ZSH_EXECUTION_STRING} ]]; then
+      exec ${pkgs.fish}/bin/fish ''${login_shell:+--login}
     fi
   '';
 }
